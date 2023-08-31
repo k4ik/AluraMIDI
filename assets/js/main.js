19ec99a99839b1cebc34dda0b7ca6b3b99dd4fdd
Kaik <121681971+k4ik@users.noreply.github.com>
2023-08-31 09:34:42 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+ListaDeTeclas[0].onclick = tocaSomPom;
